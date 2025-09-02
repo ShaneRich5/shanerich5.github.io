39878bfc0364d14063d5557b174273b6b3f14907
Shane Richards <shane.richards121@gmail.com>
2025-09-02 03:18:28 -0400

    feat: configuring github pages by adding an new deploy step and nextjs config for static rendering

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // enables static export
+  images: {
+    unoptimized: true, // Github Pages cannot optimize images
+  },
 };
 
 export default nextConfig;
